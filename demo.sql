e9f536da74d04a9769a0910f762f4a9f15971f13
马凯俊24373011 <1161668282qq.com>
2026-06-08 20:57:31 +0800

    新增直播间数据库表

--- demo.sql
+++ demo.sql
@@ -9,3 +9,16 @@ CREATE TABLE `sys_user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户基本信息表';
+
+
+--直播间表
+CREATE TABLE live_room (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  user_id BIGINT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  stream_name VARCHAR(100) NOT NULL UNIQUE,
+  push_url VARCHAR(255) NOT NULL,
+  play_url VARCHAR(255) NOT NULL,
+  status VARCHAR(20) NOT NULL DEFAULT 'offline',
+  create_time DATETIME DEFAULT CURRENT_TIMESTAMP
+);
